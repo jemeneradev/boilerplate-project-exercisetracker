fdb0586244bdb3559894d580cb443a8802fcc53d
jemeneradev <63590421+jemeneradev@users.noreply.github.com>
2020-04-15 10:38:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 
 //mongoose.connect(process.env.MONGODB)// || 'mongodb://localhost/exercise-track' )
 
-var db = mongoose.createConnection("mongodb://glitchapp:cyf4yEMjBSBBemsq@cluster0-shard-00-00-ygwgw.mongodb.net:27017,cluster0-shard-00-01-ygwgw.mongodb.net:27017,cluster0-shard-00-02-ygwgw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", {
+var db = mongoose.createConnection("", {
   useMongoClient: true
 })
 
